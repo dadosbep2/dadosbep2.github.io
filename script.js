328ad787edbe710d65a38804ca80ada624ad7ff8
dadosbep2 <dimitri.gabriel@bepdistribuidora.com>
2026-03-06 11:35:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function calcularRV() {
     let prod = parseFloat(document.getElementById("prod").value) || 0;
+	let desempenho = parseFloat(document.getElementById("desempenho").value) || 0;
     let faixa10 = parseInt(document.getElementById("10k").value) || 0;
     let faixa15 = parseInt(document.getElementById("15k").value) || 0;
     let faixa20 = parseInt(document.getElementById("20k").value) || 0;
     let faixa25 = parseInt(document.getElementById("25k").value) || 0;
     let prodb = document.getElementById("prodBonus").value.trim().toUpperCase();
     let tpvmediop = parseFloat(document.getElementById("tpvmediop").value) || 0;
-    let tpvmediotri = parseFloat(document.getElementById("tpv-medio").value) || 0;
-    let vendas = parseInt(document.getElementById("qtdvendas").value) || 0;
-    let meta = parseFloat(document.getElementById("metaconjunta").value) || 0;
+    let prodbi = parseFloat(document.getElementById("prodbi").value) || 0;
+    let tpvmediobi = parseFloat(document.getElementById("tpvmediobi").value) || 0;
+    let migracaobi = parseFloat(document.getElementById("migracaobi").value) || 0;
 
     let valorComissao = 0;
 
@@ -23,7 +24,22 @@ function calcularRV() {
 
     // Definição do valor base para premiação
     let valorBase = faixa10*12 + faixa15*20 + faixa20*37.5 + faixa25*55;
-
+	let ds = 0;
+	if (desempenho < 85) {
+		ds = 0;
+	} else if (desempenho >= 85 & desempenho < 90) {
+		ds = 0.05;
+	} else if (desempenho >= 90 & desempenho < 100) {
+		ds = 0.075;
+	} else if (desempenho >= 100 & desempenho < 120) {
+		ds = 0.1;
+	} else if (desempenho >= 120 & desempenho < 150) {
+		ds = 0.2;
+	} else if (desempenho >= 150) {
+		ds = 0.5;
+	}
+	valorBase *= (1+ds);
+	
     // Definição do valor da premiação bônus
     let valorBonus = 0;
     if (prodb == "S" && tpvmediop >= 15000) {
@@ -36,13 +52,23 @@ function calcularRV() {
 
     // Definição valor trimestral
     let valorTrimestral = 0;
-    if (tpvmediotri >= 15000) {
-	valorTrimestral += 1650;
+    if (prodbi >= 0.4) {
+		valorTrimestral += 740;
     }
-    if (vendas >= 240) {
-	valorTrimestral += 1650;
+    if (tpvmediobi >= 15000) {
+		valorTrimestral += 1480;
     }
-    valorTrimestral += (meta/100)*5500;	
+	if (migracaobi < 30) {
+		valorTrimestral += 0;
+	} else if (migracaobi >= 30 & migracaobi < 40) {
+		valorTrimestral += 370;
+	} else if (migracaobi >= 40 & migracaobi < 50) {
+		valorTrimestral += 555;
+	} else if (migracaobi >= 50 & migracaobi < 60) {
+		valorTrimestral += 925;
+	} else {
+		valorTrimestral += 1480;
+	}	
     
     // Salário fixo e ajuda de custo
     const salarioFixo = 2783.12;
@@ -74,5 +100,6 @@ function selecionarBonus(botao) {
     // Atualiza o input hidden para o cálculo
     document.getElementById("prodBonus").value = botao.dataset.value;
 }
+
 
 
